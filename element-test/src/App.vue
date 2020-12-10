fcede888a34851b5b8652e55a8b5be9d2ec0548f
cunweizhao <18500103508@163.com>
2020-12-10 15:38:40 +0800

    1.0
    element 语法练习

---
+++ element-test/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+  <el-button @click="show">按钮</el-button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  methods: {
+    show(){
+      this.$message.success('element-ui提示')
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
